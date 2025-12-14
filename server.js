bd68cd88e5b29de5687058a31cba0d842ab83496
Yannick <yannick.lebec.dev@gmail.com>
2025-12-14 14:31:00 +0100

    modif meta

--- server.js
+++ server.js
@@ -230,6 +230,7 @@ app.post("/login", async (req, res) => {
         <html lang="fr">
           <head>
             <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Level 1 – Succès</title>
             <link rel="stylesheet" href="/style.css" />
           </head>
