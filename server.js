c1809a651aec8e1a30dcbac1ad03f56f04488afd
Yannick <yannick.lebec.dev@gmail.com>
2026-02-28 10:47:49 +0100

    Fix static path for Vercel

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // === CONFIG EXPRESS / SESSIONS ===
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
